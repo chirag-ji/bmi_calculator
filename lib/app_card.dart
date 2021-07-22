97b23610269476eda1f1c5593bb57423b7474b16
Chirag <apps.chiragji@outlook.in>
2021-07-22 20:05:16 +0530

    added configurable color constructor field

--- lib/app_card.dart
+++ lib/app_card.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppCard extends StatelessWidget {
+  final Color _color;
+
+  AppCard({Color? color}) : this._color = color ?? Color(0xFF1D1E33);
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -8,7 +12,7 @@ class AppCard extends StatelessWidget {
         height: 200.0,
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
+          color: this._color,
           borderRadius: BorderRadius.circular(10.0),
         ),
       ),

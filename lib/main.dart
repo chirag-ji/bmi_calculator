fd83147543016687668bf3ea07e461844d8911c0
Chirag <apps.chiragji@outlook.in>
2021-07-22 08:49:04 +0530

    added theme and sample widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,33 @@
-import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/app.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: appName,
-    home: SafeArea(
-      child: BMICalculator(),
-    ),
-  ));
+  runApp(BMICalculator());
 }
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: App.appName,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF1E1B31),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(App.appName),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Body Text Here'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          child: Icon(Icons.admin_panel_settings),
+        ),
+      ),
+    );
   }
 }

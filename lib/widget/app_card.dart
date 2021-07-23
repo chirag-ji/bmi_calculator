a87b5e90d4d4c6f4e3c7fb75f4c284847df88c74
chirag <chirag>
2021-07-23 14:23:34 +0530

    added Gesture detector to AppCard

--- lib/widget/app_card.dart
+++ lib/widget/app_card.dart
@@ -3,20 +3,24 @@ import 'package:flutter/material.dart';
 class AppCard extends StatelessWidget {
   final Color _color;
   final Widget child;
+  final Function()? onTap;
 
-  AppCard({Color? color, required this.child})
+  AppCard({Color? color, required this.child, this.onTap})
       : this._color = color ?? Color(0xFF1D1E33);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        child: this.child,
-        height: 200.0,
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: this._color,
-          borderRadius: BorderRadius.circular(10.0),
+      child: GestureDetector(
+        onTap: this.onTap,
+        child: Container(
+          child: this.child,
+          height: 200.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color: this._color,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
         ),
       ),
     );

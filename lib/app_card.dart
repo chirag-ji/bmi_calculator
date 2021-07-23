807209ce3db6479b4810b5af03665e5a6a968867
Chirag <apps.chiragji@outlook.in>
2021-07-23 09:14:15 +0530

    added child property in AppCard

--- lib/app_card.dart
+++ lib/app_card.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 class AppCard extends StatelessWidget {
   final Color _color;
+  final Widget child;
 
-  AppCard({Color? color}) : this._color = color ?? Color(0xFF1D1E33);
+  AppCard({Color? color, required this.child})
+      : this._color = color ?? Color(0xFF1D1E33);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
+        child: this.child,
         height: 200.0,
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(

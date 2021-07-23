2ad6d453c8ec560a8b52518f5491b16a291ae209
Chirag <apps.chiragji@outlook.in>
2021-07-23 23:07:04 +0530

    constant realign

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,8 @@ class App {
   static const String appName = 'BMI Calculator';
 
   static const Color textDisplayColor = Color(0XFF8D8E98);
+  static const Color accentActiveColor = Color(0xFFEB1555);
+  static const Color accentInactiveColor = Color(0XFF8D8E98);
 
   static const TextStyle textStyle = TextStyle(
     fontSize: 18.0,
--- lib/window.dart
+++ lib/window.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const double bottomContainerHeight = 80.0;
-const Color bottomContainerColor = Color(0xFFEB1555);
 const EdgeInsets bottomContainerMargin = EdgeInsets.only(top: 10.0);
 
 const Color activeCardColor = Color(0xFF1D1E33);
@@ -72,7 +71,7 @@ class _WindowState extends State<Window> {
             ],
           ),
           Container(
-            color: bottomContainerColor,
+            color: App.accentActiveColor,
             margin: bottomContainerMargin,
             width: double.infinity,
             height: bottomContainerHeight,

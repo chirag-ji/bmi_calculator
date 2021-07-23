99a13dadd004f14db41b4fa32d84ab2b9508ac70
Chirag <apps.chiragji@outlook.in>
2021-07-23 08:44:30 +0530

    added bottom container

--- lib/window.dart
+++ lib/window.dart
@@ -1,6 +1,10 @@
 import 'package:bmi_calculator/app_card.dart';
 import 'package:flutter/material.dart';
 
+const double bottomContainerHeight = 80.0;
+const Color bottomContainerColor = Color(0xFFEB1555);
+const EdgeInsets bottomContainerMargin = EdgeInsets.only(top: 10.0);
+
 class Window extends StatefulWidget {
   const Window({Key? key}) : super(key: key);
 
@@ -29,6 +33,12 @@ class _WindowState extends State<Window> {
               AppCard(),
             ],
           ),
+          Container(
+            color: bottomContainerColor,
+            margin: bottomContainerMargin,
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
         ],
       ),
     );

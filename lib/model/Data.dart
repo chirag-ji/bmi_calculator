b31fa0fc6b55696fe1f994f4b85ea87abffad5da
Chirag <apps.chiragji@outlook.in>
2021-07-24 15:07:27 +0530

    added Data class to pass the data to resultPage

---
+++ lib/model/Data.dart
@@ -0,0 +1,10 @@
+import 'package:bmi_calculator/app.dart';
+
+class Data {
+  Gender gender;
+  double height;
+  double weight;
+  int age;
+
+  Data(this.gender, this.height, this.weight, this.age);
+}
